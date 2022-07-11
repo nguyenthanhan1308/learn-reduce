2e77deb6319bb944a31fe5547c3e52ce534e0eae
Nguyễn Thành An <sunthanhan1308@gmail.com>
2022-07-11 13:48:03 +0700

    multiple, count duplicated char

--- reducer.js
+++ reducer.js
@@ -1,5 +1,6 @@
 const numberArray = [1,2,3,4,5,6,7,8,9];
 const textArray = ["a","b","c","d","e"];
+const str = "hello world";
 const scoresObject = [
     {
         name: "A",
@@ -20,13 +21,25 @@ const scoresObject = [
 const  sumReduce = (total, number) => {
     return total + number;
 };
+const multipleReduce = (total, number) => total*number;
 const sumByProportion = (sum, y) => (sum + y.score * y.proportion);
 const maxReduce = (x,y) => Math.max(x,y);
-const numReduce = numberArray.reduce(sumReduce);
+const countDupReduce = (result,currentChar) => { 
+    result[currentChar] ? result[currentChar] ++ : result[currentChar] = 1;
+    return result;
+};
+
+
+const numReduce = numberArray.reduce( sumReduce);
 const textReduce = textArray.reduce(sumReduce);
 const numReduceProportion = scoresObject.reduce(sumByProportion, 0);
 const numMax = numberArray.reduce(maxReduce);
+const strCounted = str.split("").reduce(countDupReduce, {});
+const multipleCalc = numberArray.reduce(multipleReduce, 1);
+
 console.log(numReduce);
+console.log(multipleCalc);
 console.log(numReduceProportion);
 console.log(textReduce);
+console.log("String duplication count: ", strCounted);
 console.log("Max number of array: ",numMax);

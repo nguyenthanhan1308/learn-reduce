a5ae04bb0d83a2456912a9ca639ec072432c8ea4
Nguyễn Thành An <sunthanhan1308@gmail.com>
2022-07-11 11:56:30 +0700

    num max, object sum by key

--- reducer.js
+++ reducer.js
@@ -1,9 +1,32 @@
-const numberArray = [1,2,3,4,5,6,7,8,9]
-const textArray = ["a","b","c","d","e"]
-const  sum = (total, number) => {
+const numberArray = [1,2,3,4,5,6,7,8,9];
+const textArray = ["a","b","c","d","e"];
+const scoresObject = [
+    {
+        name: "A",
+        score: 10,
+        proportion: 30,
+    },
+    {
+        name: "B",
+        score: 20,
+        proportion: 30,
+    },
+    {
+        name: "C",
+        score: 40,
+        proportion: 40,
+    },
+];
+const  sumReduce = (total, number) => {
     return total + number;
-}
-const numReduce = numberArray.reduce(sum);
-const textReduce = textArray.reduce(sum);
-console.log(numReduce)
-console.log(textReduce)
+};
+const sumByProportion = (sum, y) => (sum + y.score * y.proportion);
+const maxReduce = (x,y) => Math.max(x,y);
+const numReduce = numberArray.reduce(sumReduce);
+const textReduce = textArray.reduce(sumReduce);
+const numReduceProportion = scoresObject.reduce(sumByProportion, 0);
+const numMax = numberArray.reduce(maxReduce);
+console.log(numReduce);
+console.log(numReduceProportion);
+console.log(textReduce);
+console.log("Max number of array: ",numMax);
